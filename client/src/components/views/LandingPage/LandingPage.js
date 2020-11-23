bc3e9eafb64ac1376e430ee6ccc79fc3bba37b02
yjcho <jinjanic91@gmail.com>
2020-11-23 22:14:34 +0900

    이미지슬라이드, 더보기버튼 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,38 +2,67 @@ import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios';
 import { Icon, Col, Card, Row, Button } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
 import Meta from 'antd/lib/card/Meta';
 import StarRatings from 'react-star-ratings';
 
 function LandingPage() {
 
     const [Products, setProducts] = useState([]);
+    const [Skip, setSkip] = useState(0);
+    const [Limit, setLimit] = useState(4);
+    const [PostSize, setPostSize] = useState(0);
 
     useEffect(() => {
 
+        //리뷰 게시글 수 컨트롤 하기 위한 변수들. (더보기 버튼 활용)
         let body = {
-
+            skip: Skip,
+            limit: Limit
         }
 
-        axios.post('/api/product/allProducts', )
+        getProducts(body)
+
+    }, [])
+
+    const getProducts = (body) => {
+        axios.post('/api/product/allProducts', body)
             .then(response => {
                 if (response.data.success) {
                     // console.log(response.data);
-                    setProducts(response.data.productsInfo)
+                    if (body.loadMore) {
+                        setProducts([...Products, ...response.data.productsInfo])
+                    } else {
+                        setProducts(response.data.productsInfo)
+                    }
+                    setPostSize(response.data.postSize)
                 }else {
                     alert('상품들을 가져오는 데 실패함.')
                 }
             } )
+    }
 
+    const loadMoreHandler = () => {
+        //현재 이후의 게시글부터 불러와야함.
+        let skip = Skip + Limit;
 
-    }, [])
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true
+        }
+
+        getProducts(body)
+        //여기서 새로 스킵을 바꾸어주어야 계속해서 다음 게시글부터 가져올 수 있음.
+        setSkip(skip)
+    }
 
     const renderCards = Products.map((product, index) => {
-        console.log(product);
+        // console.log(product);
 
         return <Col lg={6} md={8} xs={24} key={index}>
             <Card
-                cover={<img style={{width: '100%', maxHeight: '150px'}} src={`http://localhost:5000/${product.images[0]}`} />}
+                cover={<ImageSlider images={product.images} />}
                 bodyStyle={{paddingBottom: '5px'}}
             >
                 <Meta
@@ -47,7 +76,7 @@ function LandingPage() {
                         starDimension="15px"
                         starSpacing="5px"
                         />
-                        <p>{product.createdAt.slice(0, 10)}</p>
+                        <p>{product.createdAt && product.createdAt.slice(0, 10)}</p>
                     </div>]}
                 />
                 
@@ -55,6 +84,7 @@ function LandingPage() {
         </Col>
     })
 
+    
 
     return (
         <div style={{ width: '75%', margin: '3rem auto'}}>
@@ -72,9 +102,12 @@ function LandingPage() {
 
             </Row>
 
-            <div style={{ justifyContent: 'center' }}>
-                <Button>더보기</Button>
-            </div>
+            {PostSize >= Limit && 
+                <div style={{justifyContent: 'center', textAlign: 'center', marginTop: '1.5rem'}}>
+                    <Button onClick={loadMoreHandler}>더보기</Button>
+                </div>
+            }
+
 
         </div>
     )
--- server/routes/product.js
+++ server/routes/product.js
@@ -47,11 +47,20 @@ router.post('/', (req, res) => {
 router.post('/allProducts', (req, res) => {
   
   //product collection안에 들어있는 모든 상품 정보 불러오기.
+
+  //더보기를 누르기 전에 최대 몇 개 까지 보여줄지 리미트를 정함.
+  let limit = req.body.limit ? parseInt(req.body.limit) : 20;
+  //만약 스킵이 없다면 처음부터 데이터를 보여줘야 함. (만약 이미 데이터가 있다면, 그 다음 게시물부터 보여줘야함)
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+
   Product.find()
     .populate('writer') //writer에 대한 모든 정보 가져옴.
+    .skip(skip)
+    .limit(limit)
     .exec((err, productsInfo) => {
       if (err) return res.status(400).json({success: false, err})
-      return res.status(200).json({success: true, productsInfo})
+      return res.status(200).json({success: true, productsInfo, postSize: productsInfo.length})
     })
 
 })
